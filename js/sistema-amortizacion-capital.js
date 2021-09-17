b2fb0e9854073999aaed9bab9c1265b4988cab45
Eliezer Hernandez <eliezersam93@gmail.com>
2021-09-17 08:55:21 -0400

    Agregando logica basica para realizar los calculos en js

--- js/sistema-amortizacion-capital.js
+++ js/sistema-amortizacion-capital.js
@@ -1,3 +1,6 @@
+function transformTipoInteres(tipoInteres){
+    return tipoInteres / 100;
+}
 function calcularCuotaMensual(tipoInteres,capitalPendiente){
     let cuotaMensual = capitalPendiente * tipoInteres /(1-Math.pow(1+tipoInteres,-3));
     cuotaMensual= cuotaMensual.toFixed(2);
@@ -17,23 +20,31 @@ function calcularCapitalPendiente(capitalPendiente,amortizacion){
     const nuevoCapitalPendiente = (capitalPendiente - amortizacion).toFixed(2);
     return nuevoCapitalPendiente;
 }
+function validarCampos(objectPrestamo){
+    const {tipoInteres,amortizacion,cantidadMeses} = objectPrestamo;
+    return tipoInteres != 0 && amortizacion != 0 && cantidadMeses != 0;
+}
 function calcularTablaAmortizacion(tipoInteres,capitalPendiente,cantidadMeses){
-    const tablaAmortizacion = [{
+    const objectAmortizacion = {
         ano:0,
         cuotaMensual: 0,
         interes: 0,
         amortizacion: 0,
         capitalPendiente, 
-    }];
-
+    }
+    const listTablaAmortizacion = [];
+    
+    listTablaAmortizacion.push(objectAmortizacion);
+    tipoInteres = transformTipoInteres(tipoInteres);
     const cuotaMensual = calcularCuotaMensual(tipoInteres,capitalPendiente);
+    
     for(let i = 1; i <= cantidadMeses ; i++){
-        const {capitalPendiente} = tablaAmortizacion[i - 1];
+        const {capitalPendiente} = listTablaAmortizacion[i - 1];
         const ano = i;
         const interes = calcularInteres(capitalPendiente,tipoInteres);
         const amortizacion = calcularAmortizacion(cuotaMensual,interes);
         const capitalPendienteActual = calcularCapitalPendiente(capitalPendiente,amortizacion);
-        tablaAmortizacion.push({
+        listTablaAmortizacion.push({
             ano,
             cuotaMensual,
             interes,
@@ -42,7 +53,104 @@ function calcularTablaAmortizacion(tipoInteres,capitalPendiente,cantidadMeses){
         });
     }
 
-    return tablaAmortizacion;
+    return listTablaAmortizacion;
+}
+function limpiarHTML(){
+    const $results = document.querySelector("#results");
+    
+    while($results.lastElementChild.className != "label--result"){
+            $results.removeChild($results.lastElementChild);
+    }
+}
+function mostrarResultados(list){
+    const $results = document.querySelector("#results");
+    
+    limpiarHTML();
+
+    list.forEach(element => {
+        for( const key in  element){
+            let newElement = document.createElement("P");
+            newElement.textContent = element[key];
+            newElement.style.textAlign = "center";
+            $results.appendChild(newElement);
+        }
+    })
 }
 
-console.table(calcularTablaAmortizacion(0.05,6000,3));
+function capturarDatos(buttonPorcent){
+    const $campoCapitalPendiente = document.querySelector("#loan");
+    const $campoCantidadMeses = document.querySelector("#time");
+
+    const objectPrestamo = {
+        capitalPendiente: $campoCapitalPendiente.value,
+        cantidadMeses: $campoCantidadMeses.value,
+        tipoInteres: buttonPorcent,
+    };
+    console.log(objectPrestamo);
+    return objectPrestamo;
+}
+
+function validarCampo(value){
+    return value === "";
+}
+
+function alertCampo(target){
+    const value = target.value;
+    if(validarCampo(value)){
+        target.style.border = "1px solid green";
+    }else{
+        target.style.border = "1px solid red";
+    }
+}
+
+function callEvents(){
+    const $form = document.querySelector("#form");
+    let objectPrestamo = {
+        tipoInteres: 0,
+        capitalPendiente: 0,
+        amortizacion: 0,
+    };
+    let buttonPorcent = 0;
+
+    mostrarResultados([{
+        ano:0,
+        cuotaMensual: 0,
+        interes: 0,
+        amortizacion: 0,
+        capitalPendiente: 0, 
+    }]);
+
+    
+    $form.addEventListener("onkeypress",(evt) => {
+        const target = evt.target;
+        
+        if(target.id === "loan" || target.id === "time"){
+            objectPrestamo = capturarDatos(buttonPorcent);
+            if(validarCampos(objectPrestamo)){
+                let {tipoInteres,capitalPendiente,cantidadMeses} = objectPrestamo;
+                const listAmortizacion = calcularTablaAmortizacion(tipoInteres,capitalPendiente,cantidadMeses);
+                mostrarResultados(listAmortizacion);
+            }
+        }
+    });
+
+    $form.addEventListener("click",evt =>{
+        const target = evt.target;
+        if(target.id === "loan" || target.id === "time"){ 
+            alertCampo(target);
+        }else{
+            if(target.classList[0] === "button-porcent"){
+                buttonPorcent = target.value;
+                objectPrestamo = capturarDatos(buttonPorcent);
+               
+                if(validarCampos(objectPrestamo)){
+                    let {tipoInteres,capitalPendiente,cantidadMeses} = objectPrestamo;
+                    const listAmortizacion = calcularTablaAmortizacion(tipoInteres,capitalPendiente,cantidadMeses);
+                    mostrarResultados(listAmortizacion);
+                }
+            }
+        } 
+    })
+    
+}
+callEvents();
